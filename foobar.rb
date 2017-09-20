cde10199b861f8c2f8abc2b6417fb5f4414b74a3
Amanda Yuan <amandayuan@berkeley.edu>
2017-09-20 01:10:29 -0700

    hw1-amandayuan

--- foobar.rb
+++ foobar.rb
@@ -1,9 +1,15 @@
 class Foobar
-
-  def self.baz(a)
+    
+    def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
-  end
+    a = a.map {|n| n.to_i}
+    a = a.map {|n| n + 2}
+    a = a.uniq
+    a = a.delete_if {|n| n%2 != 0 || n > 10 }
+    return a.sum
 end
+end
+
 
 
--- hw1.rb
+++ hw1.rb
@@ -1,20 +1,23 @@
 def squared_sum(a, b)
-  # Q1 CODE HERE
-
+    # Q1 CODE HERE
+    sum = a + b
+    square_sum = sum**2
+    return square_sum
 end
 
 def sort_array_plus_one(a)
-  # Q2 CODE HERE
-
+    # Q2 CODE HERE
+    b = a.map {|n| n + 1}
+    return b.sort
 end
 
 def combine_name(first_name, last_name)
-  # Q3 CODE HERE
-
+    # Q3 CODE HERE
+    return first_name.concat(" ").concat(last_name)
 end
 
 def blockin_time(a)
-  # DO NOT EDIT THIS CODE BELOW
-  require './foobar'
-  Foobar.baz a
+    # DO NOT EDIT THIS CODE BELOW
+    require './foobar'
+    Foobar.baz a
 end
